cb86afc6e31fbd3cf9373a4906b54405b5eeb33b
RamonRenteria1 <24308060610072@cetis61.edu.mx>
2025-10-08 08:13:54 -0600

    C

--- app.py
+++ app.py
@@ -2,31 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')  
-def hola():
-    
-    return render_template('Plantilla_base.html')
 
-@app.route('/Inicio')
+
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('/index.html')
 
 @app.route('/acerca')
 def acerca():
-    return render_template('acerca.html')
+    return render_template('/acerca.html')
 
 
 @app.route('/animales')
 def animales():
-    return render_template('Animales.html')
+    return render_template('/Animales.html')
 
 @app.route('/vehiculos')
 def vehiculos():
-    return render_template('vehiculos.html')
+    return render_template('/vehiculos.html')
 
 @app.route('/maravillas')
 def maravillas():
-    return render_template('maravillas.html')
+    return render_template('/maravillas.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
