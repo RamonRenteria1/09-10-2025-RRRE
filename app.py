367e57256c91667c187d6de0d7466776ad7f781c
RamonRenteria1 <24308060610072@cetis61.edu.mx>
2025-10-17 08:05:00 -0600

    44523

--- app.py
+++ app.py
@@ -1,38 +1,45 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
-def Inicio():
-    return render_template('/Formulario.html')
-
-@app.route('/Iniciar Sesion')
-def sesion():
-    render_template('/IniciarSecion.html')
+def inicio():
+    return redirect(url_for('index')) 
 
 @app.route('/index')
 def index():
-    return render_template('/index.html')
+    return render_template('index.html')
+
+@app.route('/registro', methods=['GET', 'POST'])
+def registrar():
+    if request.method == 'POST':
+        nombres = request.form.get('nombres')
+        apellido = request.form.get('apellido')
+        email = request.form.get('email')
+
+        
+        print(f"Nuevo registro: {nombres} {apellido} - {email}")
+
+        # Después del registro manda al inicio
+        return redirect(url_for('index'))
+
+    return render_template('Registrar.html')
 
 @app.route('/acerca')
 def acerca():
-    return render_template('/acerca.html')
-
+    return render_template('acerca.html')
 
 @app.route('/animales')
 def animales():
-    return render_template('/Animales.html')
+    return render_template('Animales.html')
 
 @app.route('/vehiculos')
 def vehiculos():
-    return render_template('/vehiculos.html')
+    return render_template('vehiculos.html')
 
 @app.route('/maravillas')
 def maravillas():
-    return render_template('/maravillas.html')
+    return render_template('maravillas.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
